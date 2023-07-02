92bdbe1aa4a85c5536bdfc877a464e2eb37a8a9e
ionan santos <ionan564@gmail.com>
2023-07-02 18:12:27 -0300

    ajuste

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '172.19.0.2'),
+            'host' => env('DB_HOST', '172.21.0.2'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
