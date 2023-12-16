fe7fb72a605552af89da9d9216296b24c47efd05
ionansantos <ionansantos@ionansatossiMac.clickip>
2023-12-15 23:48:38 -0400

    conflict depedences

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,25 +1,23 @@
-import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/vue3'
-import { createStore } from 'vuex'
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+import { createStore } from "vuex";
 import Toaster from "@meforma/vue-toaster";
-import VueDatePicker from '@vuepic/vue-datepicker';
-import '@vuepic/vue-datepicker/dist/main.css'
+import VueDatePicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
 
-const store = createStore({
-})
-
+const store = createStore({});
 
 createInertiaApp({
-    resolve: name => {
-        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-        return pages[`./Pages/${name}.vue`]
+    resolve: (name) => {
+        const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
+        return pages[`./Pages/${name}.vue`];
     },
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(Toaster)
             .use(store)
-            .use('VueDatePicker', VueDatePicker)
+            .use("VueDatePicker", VueDatePicker)
             .use(plugin)
-            .mount(el)
+            .mount(el);
     },
-})
+});
